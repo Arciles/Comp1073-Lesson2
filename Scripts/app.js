5eca1147068047a9dd12d1c0de3eb895f6c7621e
Esat IBIS <esat.taha.ibis@gmail.com>
2016-01-21 13:47:14 -0500

    added some condition states and variables.

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,7 +2,22 @@
  * Created by esattahaibis on 2016-01-14.
  */
 
-console.log('App started ....');
+console.log("App started ....");
+
+var myName = "Esat";
+var myAge = 23;
+
+console.log(myName + "'s app started ...");
+console.log(myName + " is " + myAge);
+
+if (myAge >= 40) {
+	console.log("You are old and wise!!");
+}else {
+	console.log("You are young and energetic!1!");
+}
+
+
+
 
 
 
